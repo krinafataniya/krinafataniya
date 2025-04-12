c9427b1859e28d945ef9e6a8b97212279e289f30
krinafataniya <141635097+krinafataniya@users.noreply.github.com>
2025-04-12 09:23:05 +0530

    Create Package.java

---
+++ Package.java
@@ -0,0 +1,25 @@
+package bscit;
+class A
+{
+     public static void main(String a[])
+   {
+      System.out.println("this is data()");
+   }
+}
+
+class B extends A
+{
+    void demo()
+    {
+       System.out.println("this is demo()");
+    }
+}
+
+import bscit.*;
+public class test2
+{
+  public static void main(String a[])
+  {
+     System.out.println("this is main class");
+  }
+}
