d4082e173f3897475a9c672a3c20de9c01f92c4d
krinafataniya <141635097+krinafataniya@users.noreply.github.com>
2025-07-30 18:28:19 +0530

    Create Contactform.php

---
+++ Contactform.php
@@ -0,0 +1,7 @@
+<form action="contact.php" method="POST">
+  <input type="text" name="name" placeholder="Your Name" required>
+  <input type="email" name="email" placeholder="Your Email" required>
+  <input type="text" name="subject" placeholder="Subject" required>
+  <textarea name="message" placeholder="Message" required></textarea>
+  <button type="submit">Send Message</button>
+</form>
